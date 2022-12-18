50d0f7c7fbeb0862ddfdfc0a2a77a5c298c84db5
Aditya Upadhyay <adiupd123@gmail.com>
2022-12-18 21:13:07 +0530

    Added a bunch of Activities
    Completed TicketBookActivity, LoginActivity, RegisterActivity, PaymentActivity layouts

--- app/src/main/java/com/adiupd123/myapplication/MainActivity.kt
+++ app/src/main/java/com/adiupd123/myapplication/MainActivity.kt
@@ -2,8 +2,7 @@ package com.adiupd123.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
+import android.view.*
 import android.widget.*
 import com.adiupd123.myapplication.databinding.ActivityMainBinding
 import com.airbnb.paris.extensions.backgroundTint
@@ -33,5 +32,21 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, "Movies", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.account_item -> Toast.makeText(applicationContext,"account_item", Toast.LENGTH_SHORT).show()
+            R.id.tickets_item -> Toast.makeText(applicationContext,"tickets_item", Toast.LENGTH_SHORT).show()
+            R.id.contact_item -> Toast.makeText(applicationContext,"contact_item", Toast.LENGTH_SHORT).show()
+            R.id.logout_item -> Toast.makeText(applicationContext,"logout_item", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
---
+++ app/src/main/java/com/adiupd123/myapplication/TicketBookActivity.kt
@@ -0,0 +1,11 @@
+package com.adiupd123.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TicketBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticketbook)
+    }
+}
